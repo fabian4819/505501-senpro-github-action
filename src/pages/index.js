5775a4f2d6bd1ef05b50df7ca974a8a6bcfcb575
fabian4819 <habibfabianfahlesi2004@mail.ugm.ac.id>
2025-03-13 13:53:05 +0700

    fix: index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
 
         <div>
           <h5 className={inter.className}>
-            [Nama] | [NIU]
+            Habib Fabian Fahlesi | 505501
           </h5>
         </div>
       </main>
